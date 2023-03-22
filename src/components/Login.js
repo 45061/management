61b7a09431d5a9d4a1ec9a3e9f35c100cfb4774b
Anderson <andeersonr@gmail.com>
2023-03-22 18:36:51 -0500

    store ok

---
+++ src/components/Login.js
@@ -0,0 +1,83 @@
+import Image from "next/image";
+import { useState } from "react";
+import styles from "../styles/components/Login.module.scss";
+
+import { useDispatch } from "react-redux";
+
+// import axios from "axios";
+import Link from "next/link";
+import { login } from "@/store/actions/authAction";
+
+export default function Login() {
+  const dispatch = useDispatch();
+
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChange = (event) => {
+    setLoginData({
+      ...loginData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    dispatch(login(loginData));
+  };
+
+  return (
+    <form className={styles.login}>
+      <header className={styles.login__header}>
+        <div className={styles.login__brand}>
+          <Image
+            src="/gestion.svg"
+            alt="NominaApp Logo"
+            width={150}
+            height={150}
+          />
+        </div>
+
+        <h3 className={styles.login__title}> Iniciar Sesión </h3>
+      </header>
+      <div className={styles.login__content}>
+        {/* Email */}
+        <input
+          name="email"
+          type="email"
+          value={loginData.name}
+          onChange={onChange}
+          className={styles.login__input}
+          placeholder="email"
+          errormessage="EL correo es requerido."
+          required
+        />
+
+        {/* Password */}
+        <input
+          name="password"
+          type="password"
+          value={loginData.name}
+          onChange={onChange}
+          className={styles.login__input}
+          placeholder="Contraseña"
+          errormessage="Minimo 8 caracteres e incluir 1 numero y 1 caracter especial"
+          pattern="^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$"
+          required
+        />
+      </div>
+      <div className={styles.login__footer}>
+        <button
+          className={styles.btn_action}
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Acceder
+        </button>
+      </div>
+    </form>
+  );
+}
